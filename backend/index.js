f0dab6eeb734d14bac529d5c1676971fd3c1e182
jeremyc293 <jeremyc293@gmail.com>
2026-08-03 10:34:04 -0400

    backend error handling

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import errorHandler from "./middleware/errorHandler.js";
 
 dotenv.config();
 
@@ -19,6 +20,16 @@ app.get("/", (req, res) => {
     message: "PlaylistHub API is running!",
   });
 });
+
+// 404 Route
+app.use((req, res) => {
+  res.status(404).json({
+    message: "Route not found.",
+  });
+});
+
+// Error Handler
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
