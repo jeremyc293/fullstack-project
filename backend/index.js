e9bc6f134f52c9db94708409f7c1142b0021497b
jeremyc293 <jeremyc293@gmail.com>
2026-08-04 10:59:12 -0400

    authentication middleware testing

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import errorHandler from "./middleware/errorHandler.js";
 import authRoutes from "./routes/authRoutes.js";
+import authMiddleware from "./middleware/authMiddleware.js";
+
 
 dotenv.config();
 
@@ -21,6 +23,13 @@ app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => {
   res.json({
     message: "PlaylistHub API is running!",
+  });
+});
+
+app.get("/api/protected", authMiddleware, (req, res) => {
+  res.json({
+    message: "Protected route accessed successfully.",
+    user: req.user,
   });
 });
 
