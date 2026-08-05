7ab81e93aed8d0c9ecfca6ba8d54d55dd1c6bcd7
jeremyc293 <jeremyc293@gmail.com>
2026-08-05 00:16:39 -0400

    home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,23 @@
 function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <div>
+      <h1>Welcome to PlaylistHub</h1>
+
+      <p>
+        PlaylistHub lets you create playlists and organize your favorite music.
+      </p>
+
+      <h2>Features</h2>
+
+      <ul>
+        <li>Create playlists</li>
+        <li>Edit playlists</li>
+        <li>Delete playlists</li>
+        <li>Search for songs</li>
+        <li>Save songs to playlists</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Home;
