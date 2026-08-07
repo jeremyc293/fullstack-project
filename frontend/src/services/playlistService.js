11a262c3e2b93cb252121909d59830f09013795c
jeremyc293 <jeremyc293@gmail.com>
2026-08-07 09:40:28 -0400

    preparing netlify

--- frontend/src/services/playlistService.js
+++ frontend/src/services/playlistService.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000/api/playlists";
+const BASE_URL = `${import.meta.env.VITE_API_URL}/api/playlists`;
 
 export async function getPlaylists() {
   const token = localStorage.getItem("token");
